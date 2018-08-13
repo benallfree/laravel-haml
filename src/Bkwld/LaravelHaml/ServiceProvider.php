e37449325ac36bbb1b82b370eaade8aabc234674
Ben Allfree <ben@benallfree.com>
2018-08-13 04:22:56 -0700

    Refert service provider fix

--- src/Bkwld/LaravelHaml/ServiceProvider.php
+++ src/Bkwld/LaravelHaml/ServiceProvider.php
@@ -75,11 +75,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider {
 		// Register compilers
 		$this->registerHamlCompiler();
 		$this->registerHamlBladeCompiler();
-
-		$customDirectives = $this->app['blade.compiler']->getCustomDirectives();
-		foreach ($customDirectives as $name => $closure) {
-			$this->app['Bkwld\LaravelHaml\HamlBladeCompiler']->directive($name, $closure);
-		}		
 	}
 
 	/**
